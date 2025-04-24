642dfcf6f69e4b36d53b7cf1ee99d39946c33864
vishnupouley <vishnupouley@gmail.com>
2025-04-24 09:28:31 +0530

    added sorting table

--- expenses_log/urls.py
+++ expenses_log/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('month-log/', include('month_log.urls')),
     path("__reload__", include("django_browser_reload.urls")),
+    path('month-log/', include('month_log.urls')),
+    path('sampletable/', include('sampletable.urls')),
 ]
---
+++ sampletable/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SampletableConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sampletable'
